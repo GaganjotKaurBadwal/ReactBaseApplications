963ebc1beecd395d7796a4c76e795852b78f0f17
GaganjotKaurBadwal <1032222170@mitwpu.edu.in>
2024-10-13 22:51:17 +0530

    Added React Form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserForm from './LoginForm.jsx';
+import './App.css'; // Add custom styles if needed
+
+function App() {
+    return (
+        <div className="App">
+            <UserForm />
+        </div>
+    );
+}
+
+export default App;
